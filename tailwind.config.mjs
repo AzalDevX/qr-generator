ab7fa7ee69a15369b243d2686e85f8255d69fcbe
Gonzalo Azaldegi <98758892+AzalDevX@users.noreply.github.com>
2024-10-20 15:33:02 +0200

    Initial commit

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,29 +3,10 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 	  extend: {
-		colors: {
-		  background: '#1c1917',
-		  card: '#292524',
-		  text: {
-			primary: '#e7e5e4',
-			secondary: '#d6d3d1',
+		fontFamily: {
+			mono: ['"Roboto Mono"', 'monospace'],
+			dotted: ['"Edu AU VIC WA NT Dots"', 'cursive'],
 		  },
-		  accent: {
-			primary: '#fb923c',
-			secondary: '#fdba74',
-		  },
-		  purple: {
-			300: '#d8b4fe',  // Asumiendo un tono similar al texto "Astro"
-		  },
-		  teal: {
-			300: '#5eead4',  // Asumiendo un tono similar al texto "TailwindCSS"
-		  },
-		  code: {
-			background: '#332e2a',
-			border: '#2b2623',
-		  },
-		  footer: '#4b5563',  // Equivalente a text-gray-600
-		},
 	  },
 	},
 	plugins: [],
